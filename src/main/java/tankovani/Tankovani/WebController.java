2cdf22fb4716d298dac09249a7d7da2754e3542a
Radim Nyč <rychleji@gmail.com>
2019-07-19 04:00:58 +0200

    Přidáno mazání a tabulka s infem o tankování

--- src/main/java/tankovani/Tankovani/WebController.java
+++ src/main/java/tankovani/Tankovani/WebController.java
@@ -3,6 +3,7 @@ package tankovani.Tankovani;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
@@ -14,10 +15,26 @@ public class WebController {
     }
     
     @GetMapping("/cars")
-    public String greeting(Model model) {
+    public String cars(Model model) {
         
         model.addAttribute("cars", database.findAllCars());
         return "cars";
+    }
+    
+    @GetMapping("/fuelling")
+    public String fuelling(@RequestParam(name="car", required=true) String licence, Model model) {
+        
+        model.addAttribute("fuellings", database.findFuellingsByCar(licence));
+        return "fuelling";
+    }
+    
+    @GetMapping("/remove")
+    public String remove(@RequestParam(name="car", required=false, defaultValue = "") String licence, @RequestParam(name="fuel", required=false, defaultValue = "-1") String id, Model model) {
+        if (!"".equals(licence))
+            database.removeCar(licence);
+        if(!"-1".equals(id))
+            database.removeFuelling(Long.parseLong(id));
+        return "remove";
     }
 
 }
